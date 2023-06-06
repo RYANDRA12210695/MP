344fd8efacdb0e6ab69fec6988617a33300b50ad
12210426-Yusuf-Aristokrat <yusufaristokrat@gmail.com>
2023-06-06 20:57:24 +0700

    perbaikan

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -1,390 +1,129 @@
-      // Figma Flutter Generator SettngsWidget - FRAME
-      Container(
-      width: 390,
-      height: 844,
+import 'package:flutter/material.dart';
+
+class SettingPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Settings'),
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          width: 390,
+          height: 844,
+          color: Color.fromRGBO(255, 255, 255, 1),
+          child: Column(
+            children: <Widget>[
+              Container(
+                width: 390,
+                height: 89,
+                margin: EdgeInsets.only(top: 20),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Color.fromRGBO(0, 0, 0, 0.25),
+                      offset: Offset(0, 10),
+                      blurRadius: 15,
+                    ),
+                  ],
+                  color: Color.fromRGBO(70, 121, 255, 1),
+                ),
+                child: Center(
+                  child: Text(
+                    'Settings',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontFamily: 'Gudea',
+                      fontSize: 24,
+                      letterSpacing: 0,
+                      fontWeight: FontWeight.normal,
+                      height: 1,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              SettingItem(
+                title: 'Settings #1',
+                description: 'description',
+              ),
+              SizedBox(height: 20),
+              SettingItem(
+                title: 'Settings #2',
+                description: 'description',
+              ),
+              SizedBox(height: 20),
+              SettingItem(
+                title: 'Settings #3',
+                description: 'description',
+              ),
+              SizedBox(height: 20),
+              SettingItem(
+                title: 'Settings #4',
+                description: 'description',
+              ),
+              SizedBox(height: 20),
+              SettingItem(
+                title: 'Settings #5',
+                description: 'description',
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SettingItem extends StatelessWidget {
+  final String title;
+  final String description;
+
+  SettingItem({required this.title, required this.description});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 365,
+      height: 77,
       decoration: BoxDecoration(
-          color : Color.fromRGBO(255, 255, 255, 1),
-  ),
-      child: Stack(
+        borderRadius: BorderRadius.circular(10),
+        color: Colors.white,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          Positioned(
-        top: -13,
-        left: 0,
-        child: Container(
-        width: 390,
-        height: 89,
-        decoration: BoxDecoration(
-          borderRadius : BorderRadius.only(
-            topLeft: Radius.circular(10),
-            topRight: Radius.circular(10),
-            bottomLeft: Radius.circular(10),
-            bottomRight: Radius.circular(10),
+          Padding(
+            padding: EdgeInsets.only(left: 15, top: 10),
+            child: Text(
+              title,
+              style: TextStyle(
+                color: Color.fromRGBO(69, 69, 69, 1),
+                fontFamily: 'Gudea',
+                fontSize: 20,
+                letterSpacing: 0,
+                fontWeight: FontWeight.normal,
+                height: 1,
+              ),
+            ),
           ),
-      boxShadow : [BoxShadow(
-          color: Color.fromRGBO(0, 0, 0, 0.25),
-          offset: Offset(0,10),
-          blurRadius: 15
-      )],
-      color : Color.fromRGBO(70, 121, 255, 1),
-  )
-      )
-      ),Positioned(
-        top: 769,
-        left: 0,
-        child: Container(
-        width: 390,
-        height: 101,
-        decoration: BoxDecoration(
-          borderRadius : BorderRadius.only(
-            topLeft: Radius.circular(10),
-            topRight: Radius.circular(10),
-            bottomLeft: Radius.circular(10),
-            bottomRight: Radius.circular(10),
+          Padding(
+            padding: EdgeInsets.only(left: 15, top: 5),
+            child: Text(
+              description,
+              style: TextStyle(
+                color: Color.fromRGBO(69, 69, 69, 1),
+                fontFamily: 'Gudea',
+                fontSize: 14,
+                letterSpacing: 0,
+                fontWeight: FontWeight.normal,
+                height: 1,
+              ),
+            ),
           ),
-      boxShadow : [BoxShadow(
-          color: Color.fromRGBO(0, 0, 0, 0.25),
-          offset: Offset(0,4),
-          blurRadius: 4
-      )],
-      color : Color.fromRGBO(56, 56, 56, 1),
-  )
-      )
-      ),Positioned(
-        top: 23,
-        left: 25,
-        child: Text('Settings', textAlign: TextAlign.left, style: TextStyle(
-        color: Color.fromRGBO(255, 255, 255, 1),
-        fontFamily: 'Gudea',
-        fontSize: 24,
-        letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
-        fontWeight: FontWeight.normal,
-        height: 1
-      ),)
-      ),Positioned(
-        top: 92,
-        left: 14,
-        child: Container(
-      width: 365,
-      height: 77,
-      
-      child: Stack(
-        children: <Widget>[
-          Positioned(
-        top: 0,
-        left: 0,
-        child: Container(
-        width: 365,
-        height: 77,
-        decoration: BoxDecoration(
-          borderRadius : BorderRadius.only(
-            topLeft: Radius.circular(10),
-            topRight: Radius.circular(10),
-            bottomLeft: Radius.circular(10),
-            bottomRight: Radius.circular(10),
-          ),
-      color : Color.fromRGBO(255, 255, 255, 1),
-  )
-      )
-      ),Positioned(
-        top: 30,
-        left: 11,
-        child: Text('description', textAlign: TextAlign.left, style: TextStyle(
-        color: Color.fromRGBO(69, 69, 69, 1),
-        fontFamily: 'Gudea',
-        fontSize: 14,
-        letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
-        fontWeight: FontWeight.normal,
-        height: 1
-      ),)
-      ),Positioned(
-        top: 4,
-        left: 11,
-        child: Text('Settings #1', textAlign: TextAlign.left, style: TextStyle(
-        color: Color.fromRGBO(69, 69, 69, 1),
-        fontFamily: 'Gudea',
-        fontSize: 20,
-        letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
-        fontWeight: FontWeight.normal,
-        height: 1
-      ),)
+        ],
       ),
-        ]
-      )
-    )
-      ),Positioned(
-        top: 185,
-        left: 14,
-        child: Container(
-      width: 365,
-      height: 77,
-      
-      child: Stack(
-        children: <Widget>[
-          Positioned(
-        top: 0,
-        left: 0,
-        child: Container(
-        width: 365,
-        height: 77,
-        decoration: BoxDecoration(
-          borderRadius : BorderRadius.only(
-            topLeft: Radius.circular(10),
-            topRight: Radius.circular(10),
-            bottomLeft: Radius.circular(10),
-            bottomRight: Radius.circular(10),
-          ),
-      color : Color.fromRGBO(255, 255, 255, 1),
-  )
-      )
-      ),Positioned(
-        top: 30,
-        left: 11,
-        child: Text('description', textAlign: TextAlign.left, style: TextStyle(
-        color: Color.fromRGBO(69, 69, 69, 1),
-        fontFamily: 'Gudea',
-        fontSize: 14,
-        letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
-        fontWeight: FontWeight.normal,
-        height: 1
-      ),)
-      ),Positioned(
-        top: 4,
-        left: 11,
-        child: Text('Settings #2', textAlign: TextAlign.left, style: TextStyle(
-        color: Color.fromRGBO(69, 69, 69, 1),
-        fontFamily: 'Gudea',
-        fontSize: 20,
-        letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
-        fontWeight: FontWeight.normal,
-        height: 1
-      ),)
-      ),
-        ]
-      )
-    )
-      ),Positioned(
-        top: 278,
-        left: 14,
-        child: Container(
-      width: 365,
-      height: 77,
-      
-      child: Stack(
-        children: <Widget>[
-          Positioned(
-        top: 0,
-        left: 0,
-        child: Container(
-        width: 365,
-        height: 77,
-        decoration: BoxDecoration(
-          borderRadius : BorderRadius.only(
-            topLeft: Radius.circular(10),
-            topRight: Radius.circular(10),
-            bottomLeft: Radius.circular(10),
-            bottomRight: Radius.circular(10),
-          ),
-      color : Color.fromRGBO(255, 255, 255, 1),
-  )
-      )
-      ),Positioned(
-        top: 30,
-        left: 11,
-        child: Text('description', textAlign: TextAlign.left, style: TextStyle(
-        color: Color.fromRGBO(69, 69, 69, 1),
-        fontFamily: 'Gudea',
-        fontSize: 14,
-        letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
-        fontWeight: FontWeight.normal,
-        height: 1
-      ),)
-      ),Positioned(
-        top: 4,
-        left: 11,
-        child: Text('Settings #3', textAlign: TextAlign.left, style: TextStyle(
-        color: Color.fromRGBO(69, 69, 69, 1),
-        fontFamily: 'Gudea',
-        fontSize: 20,
-        letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
-        fontWeight: FontWeight.normal,
-        height: 1
-      ),)
-      ),
-        ]
-      )
-    )
-      ),Positioned(
-        top: 371,
-        left: 14,
-        child: Container(
-      width: 365,
-      height: 77,
-      
-      child: Stack(
-        children: <Widget>[
-          Positioned(
-        top: 0,
-        left: 0,
-        child: Container(
-        width: 365,
-        height: 77,
-        decoration: BoxDecoration(
-          borderRadius : BorderRadius.only(
-            topLeft: Radius.circular(10),
-            topRight: Radius.circular(10),
-            bottomLeft: Radius.circular(10),
-            bottomRight: Radius.circular(10),
-          ),
-      color : Color.fromRGBO(255, 255, 255, 1),
-  )
-      )
-      ),Positioned(
-        top: 30,
-        left: 11,
-        child: Text('description', textAlign: TextAlign.left, style: TextStyle(
-        color: Color.fromRGBO(0, 0, 0, 1),
-        fontFamily: 'Gudea',
-        fontSize: 14,
-        letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
-        fontWeight: FontWeight.normal,
-        height: 1
-      ),)
-      ),Positioned(
-        top: 4,
-        left: 11,
-        child: Text('Settings #4', textAlign: TextAlign.left, style: TextStyle(
-        color: Color.fromRGBO(69, 69, 69, 1),
-        fontFamily: 'Gudea',
-        fontSize: 20,
-        letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
-        fontWeight: FontWeight.normal,
-        height: 1
-      ),)
-      ),
-        ]
-      )
-    )
-      ),Positioned(
-        top: 464,
-        left: 14,
-        child: Container(
-      width: 365,
-      height: 77,
-      
-      child: Stack(
-        children: <Widget>[
-          Positioned(
-        top: 0,
-        left: 0,
-        child: Container(
-        width: 365,
-        height: 77,
-        decoration: BoxDecoration(
-          borderRadius : BorderRadius.only(
-            topLeft: Radius.circular(10),
-            topRight: Radius.circular(10),
-            bottomLeft: Radius.circular(10),
-            bottomRight: Radius.circular(10),
-          ),
-      color : Color.fromRGBO(255, 255, 255, 1),
-  )
-      )
-      ),Positioned(
-        top: 30,
-        left: 11,
-        child: Text('description', textAlign: TextAlign.left, style: TextStyle(
-        color: Color.fromRGBO(69, 69, 69, 1),
-        fontFamily: 'Gudea',
-        fontSize: 14,
-        letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
-        fontWeight: FontWeight.normal,
-        height: 1
-      ),)
-      ),Positioned(
-        top: 4,
-        left: 11,
-        child: Text('Settings #5', textAlign: TextAlign.left, style: TextStyle(
-        color: Color.fromRGBO(69, 69, 69, 1),
-        fontFamily: 'Gudea',
-        fontSize: 20,
-        letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
-        fontWeight: FontWeight.normal,
-        height: 1
-      ),)
-      ),
-        ]
-      )
-    )
-      ),Positioned(
-        top: 783,
-        left: 25,
-        child: Container(
-      width: 48,
-      height: 48,
-      decoration: BoxDecoration(
-          color : Color.fromRGBO(255, 255, 255, 1),
-  ),
-      child: Stack(
-        children: <Widget>[
-          Positioned(
-        top: 0,
-        left: 0,
-        child: Container(
-      width: 48,
-      height: 48,
-      
-      child: Stack(
-        children: <Widget>[
-          Positioned(
-        top: 9,
-        left: 15,
-        child: SvgPicture.asset(
-        'assets/images/vector.svg',
-        semanticsLabel: 'vector'
-      );
-      ),Positioned(
-        top: 0,
-        left: 0,
-        child: SvgPicture.asset(
-        'assets/images/vector.svg',
-        semanticsLabel: 'vector'
-      );
-      ),
-        ]
-      )
-    )
-      ),
-        ]
-      )
-    )
-      ),Positioned(
-        top: 783,
-        left: 311,
-        child: Container(
-      width: 46,
-      height: 46,
-      
-      child: Stack(
-        children: <Widget>[
-          Positioned(
-        top: 13.667156219482422,
-        left: 13.667165756225586,
-        child: SvgPicture.asset(
-        'assets/images/icon/vector.svg',
-        semanticsLabel: 'vector'
-      );
-      ),Positioned(
-        top: 0,
-        left: 0,
-        child: SvgPicture.asset(
-        'assets/images/vector.svg',
-        semanticsLabel: 'vector'
-      );
-      ),
-        ]
-      )
-    )
-      ),
-        ]
-      )
-    )
+    );
+  }
+}
